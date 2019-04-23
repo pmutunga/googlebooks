bb228895d0327d56da726e7d566ad8069a35b85f
pmutunga <paskwa.mutunga@ipsafari.com>
2019-04-23 14:19:53 -0400

    heroku build

---
+++ client/build/precache-manifest.5b739aee9bc5b9ccfb7e6e8c8d85880e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be3062159a21e95f2a50c33606e496f2",
+    "url": "/static/media/hero.be306215.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f00bfcbdde3a040332d",
+    "url": "/static/js/main.53e322b7.chunk.js"
+  },
+  {
+    "revision": "f4a987eebbaa7396609c",
+    "url": "/static/js/2.0bf04e20.chunk.js"
+  },
+  {
+    "revision": "4f00bfcbdde3a040332d",
+    "url": "/static/css/main.cbdbe499.chunk.css"
+  },
+  {
+    "revision": "dbd1d3aaa63d4870ebed5c84b43f21b7",
+    "url": "/index.html"
+  }
+];
